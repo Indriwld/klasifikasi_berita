81cb4a6144e0e19535c35a62548a127dda719128
indriwld <indri_240@smkassalaambandung.sch.id>
2021-12-23 10:52:48 +0700

    hari ke 4

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +16,31 @@ class Userseeder extends Seeder
      */
     public function run()
     {
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator',
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa',
+
+        ]);
+
         $user = new User();
         $user->name = 'indriwld';
         $user->email = 'admin@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
+
+        $pengunjung = new User();
+        $pengunjung->name = 'pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
+
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
 
     }
 }
